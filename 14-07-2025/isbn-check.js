6c5bb4792ed5eaff14617029850046e302b7a0e4
MohammadAzeem Wani <mohammadazeemwani@mrma.local>
2025-07-14 15:36:35 +0530

    ISBN check updated

--- 14-07-2025/isbn-check.js
+++ 14-07-2025/isbn-check.js
@@ -1,3 +1,13 @@
 const isbn_regex = /^(?=(?:[^0-9]*[0-9]){10}(?:(?:[^0-9]*[0-9]){3})?$)[\d-]+$/;
 
+const readline = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+readline.question('Write ISBN: ', isbn => {
+  console.log(`Check ${isbn_regex.test(isbn)}!`);
+  readline.close();
+});
+
 
